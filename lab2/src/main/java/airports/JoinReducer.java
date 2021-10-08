076967c49deacdfdb8817fb6b3c6b4333645a9f7
VillKoi <t.m.volnova@gmail.com>
2021-10-08 18:25:43 +0300

    Scripted auto-commit on change (2021-10-08 18:25:43) by gitwatch.sh

--- lab2/src/main/java/airports/JoinReducer.java
+++ lab2/src/main/java/airports/JoinReducer.java
@@ -1,2 +1,10 @@
 package  airports;
 
+public class JoinReducer extends Reducer<TextPair, Text, Text, Text> {
+    @Override
+    protected void reduce(TextPair key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        Iterator<Text> iter = values.iterator(); Text systemInfo = new Text(iter.next()); while (iter.hasNext()) {
+            Text call = iter.next();
+            Text outValue = new Text(call.toString() + "\t" + systemInfo.toString()); context.write(key.getFirst(), outValue);
+        } }
+}
