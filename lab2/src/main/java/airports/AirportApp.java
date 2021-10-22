198896d7b98ffb3ca02a3f0deaa413380273ebdf
VillKoi <t.m.volnova@gmail.com>
2021-10-23 00:17:39 +0300

    Scripted auto-commit on change (2021-10-23 00:17:39) by gitwatch.sh

--- lab2/src/main/java/airports/AirportApp.java
+++ lab2/src/main/java/airports/AirportApp.java
@@ -1,6 +1,5 @@
 package airports;
 
-import com.opencsv.exceptions.CsvException;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 
@@ -9,24 +8,16 @@ import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.util.List;
-
-import com.opencsv.CSVReader;
 
 public class AirportApp {
     public static void main(String[] args) throws Exception {
-        if (args.length != 2) {
+        if (args.length != 3) {
             System.err.println("Usage: AirportsApp <1: input path FlightMapper> <2: input path AirportMapper> <output path>");
             System.exit(-1);
         }
         Job job = Job.getInstance();
         job.setJarByClass(AirportApp.class);
         job.setJobName("JoinJob sort");
-
-        List<String[]> list = ReadCSV(args[0]);
 
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class);
@@ -42,17 +33,5 @@ public class AirportApp {
         job.setOutputValueClass(Text.class);
         job.setNumReduceTasks(2);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
-    }
-
-    public static List<String[]> ReadCSV(String path) throws IOException, CsvException {
-        Reader reader = new FileReader(path);
-        CSVReader csvReader = new CSVReader(reader);
-
-        List<String[]> list = csvReader.readAll();
-
-        reader.close();
-        csvReader.close();
-
-        return list;
     }
 }
