411928ab10c85e7e70df861a21392df1f90ba2dc
VillKoi <t.m.volnova@gmail.com>
2021-10-22 20:49:54 +0300

    Scripted auto-commit on change (2021-10-22 20:49:54) by gitwatch.sh

--- lab2/src/main/java/airports/AirportWritableComparable.java
+++ lab2/src/main/java/airports/AirportWritableComparable.java
@@ -1,4 +1,17 @@
 package airports;
 
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
 public class AirportWritableComparable implements WritableComparator {
+    public void write(DataOutput var1) throws IOException{
+
+    };
+
+    public void readFields(DataInput var1) throws IOException{
+
+    };
 }
