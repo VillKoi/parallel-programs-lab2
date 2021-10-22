06a75879f2266df72dc1338650cff1d8b18c28a7
VillKoi <t.m.volnova@gmail.com>
2021-10-23 01:02:50 +0300

    Scripted auto-commit on change (2021-10-23 01:02:50) by gitwatch.sh

--- lab2/src/main/java/airports/GroupingComparator.java
+++ lab2/src/main/java/airports/GroupingComparator.java
@@ -4,6 +4,10 @@ import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 public class GroupingComparator extends WritableComparator {
+    public GroupingComparator() {
+        super(AirportWritableComparable.class, true);
+    }
+
     @Override
     public int compare(WritableComparable a, WritableComparable b) {
         AirportWritableComparable airport1 = (AirportWritableComparable) a;
