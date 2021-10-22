a8070864c57f2529be56d1ecf1f0b1231a8bb6ca
VillKoi <t.m.volnova@gmail.com>
2021-10-22 22:44:34 +0300

    Scripted auto-commit on change (2021-10-22 22:44:34) by gitwatch.sh

--- lab2/src/main/java/airports/FlightMapper.java
+++ lab2/src/main/java/airports/FlightMapper.java
@@ -10,10 +10,6 @@ import java.io.IOException;
 public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        // YEAR	QUARTER	MONTH	DAY_OF_MONTH	DAY_OF_WEEK	FL_DATE	UNIQUE_CARRIER	AIRLINE_ID	CARRIER	TAIL_NUM	FL_NUM	ORIGIN_AIRPORT_ID	ORIGIN_AIRPORT_SEQ_ID	ORIGIN_CITY_MARKET_ID	DEST_AIRPORT_ID	WHEELS_ON	ARR_TIME	ARR_DELAY	ARR_DELAY_NEW	CANCELLED	CANCELLATION_CODE	AIR_TIME	DISTANCE
-        //2015	1	1	10	6	2015-01-10	AA	19805	AA	N790AA	1	12478	1247802	31703	12892	1225	1235	0.00	0.00	0.00		345.00	2475.00
-//        < разбивает csv записываем нужную инфу
-//            в контекст пишется пара — Text и IntWritable >
         // DEST_AEROPORT_ID имеет номер 14
 
         String text  =   value.toString();
