ab9b7769b812145552e6043af9d9d3abc80e7032
VillKoi <t.m.volnova@gmail.com>
2021-10-22 20:37:36 +0300

    Scripted auto-commit on change (2021-10-22 20:37:36) by gitwatch.sh

--- lab2/src/main/java/airports/GroupingComparator.java
+++ lab2/src/main/java/airports/GroupingComparator.java
@@ -4,6 +4,7 @@ import org.apache.hadoop.io.RawComparator;
 import org.apache.hadoop.io.WritableComparable;
 
 public class GroupingComparator implements RawComparator {
+    @Override
     public int compare(WritableComparable a, WritableComparable b) {
         return a.compareTo(b);
     }
