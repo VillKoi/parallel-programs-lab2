08f9671ecfb162ba6706a1d1bdb89377a7990b5d
VillKoi <t.m.volnova@gmail.com>
2021-10-23 01:31:19 +0300

    Scripted auto-commit on change (2021-10-23 01:31:18) by gitwatch.sh

--- lab2/src/main/java/airports/AirportPartitioner.java
+++ lab2/src/main/java/airports/AirportPartitioner.java
@@ -3,7 +3,7 @@ package airports;
 
 import org.apache.hadoop.mapreduce.Partitioner;
 
-public abstract class AirportPartitioner<KEY, VALUE> extends Partitioner<KEY, VALUE> {
+public class AirportPartitioner<KEY, VALUE> extends Partitioner<KEY, VALUE> {
     @Override
     public int getPartition(KEY key, VALUE value, int numReduceTasks) {
         return (key.hashCode() & Integer.MAX_VALUE) % numReduceTasks;
