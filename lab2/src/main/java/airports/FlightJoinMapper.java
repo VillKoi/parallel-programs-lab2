65bdbe1b32470f0e50d3158e435873f64db3d4d7
VillKoi <t.m.volnova@gmail.com>
2021-10-22 19:18:40 +0300

    Scripted auto-commit on change (2021-10-22 19:18:40) by gitwatch.sh

--- lab2/src/main/java/airports/FlightJoinMapper.java
+++ lab2/src/main/java/airports/FlightJoinMapper.java
@@ -1,4 +1,21 @@
 package airports;
 
-public class FlightJoinMapper {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class FlightJoinMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+//        < разбивает csv записываем нужную инфу
+//            в контекст пишется пара — Text и IntWritable >
+        String text  =   value.toString();
+        String[] words =  text.replaceAll("[\\p{Punct}«».]", "").toLowerCase().split("\\s");
+        for (String word: words) {
+            context.write(new Text(word), new IntWritable(1));
+        }
+    }
 }
