8710327da69afcee07e7516d206a969f0d3fffd1
VillKoi <t.m.volnova@gmail.com>
2021-10-23 02:35:21 +0300

    Scripted auto-commit on change (2021-10-23 02:35:21) by gitwatch.sh

--- lab2/src/main/java/airports/JoinReducer.java
+++ lab2/src/main/java/airports/JoinReducer.java
@@ -15,7 +15,7 @@ public class JoinReducer extends Reducer<AirportWritableComparable, Text, Text, 
 
         Iterator<Text> iter = values.iterator();
         Text airportName = new Text(iter.next());
-        float minTime = MAX, maxTime = -1, meanTime = -1;
+        float minTime = Float.MAX_VALUE, maxTime = 0, meanTime = 0;
         int number = 0;
 
         while (iter.hasNext()) {
