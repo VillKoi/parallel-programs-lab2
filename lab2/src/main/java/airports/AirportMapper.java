325fc2afd390d8833ef4ed3caee339025c4fc51c
VillKoi <t.m.volnova@gmail.com>
2021-10-23 13:35:50 +0300

    Scripted auto-commit on change (2021-10-23 13:35:50) by gitwatch.sh

--- lab2/src/main/java/airports/AirportMapper.java
+++ lab2/src/main/java/airports/AirportMapper.java
@@ -9,7 +9,8 @@ import java.io.IOException;
 public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
     private static final String FIRST_STRING_PART = "Code";
     private static final String STRING_SPLITTER = "\",\"";
-    private static final String STRING_CLEANER = "Code";
+    private static final String EMPTY_STRING = "Code";
+    private static final String DOUBLE_QUOTES = "Code";
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
@@ -23,5 +24,9 @@ public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableCom
         String airportName = values[1].replaceAll("\"", "");
 
         context.write(new AirportWritableComparable(airportID, 0), new Text(airportName));
+    }
+
+    private static String removeDoubleQuotes(String value) {
+        return value.replaceAll("\"", EMPTY_STRING);
     }
 }
